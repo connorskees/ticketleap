e72484668bc524f9cfc43c851e7b66a36cda12f0
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-06-29 01:46:59 -0400

    blah

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
 from distutils.core import setup
-
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
 setup(
     name='ticketleap',
     packages=['ticketleap'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description=
     'Unofficial TicketLeap API. Create and modify TicketLeap events at scale',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     author='Connor Skees',
     author_email='connor1skees@gmail.com',
     url='https://github.com/connorskees/ticketleap',
     download_url=
-    'https://github.com/ConnorSkees/ticketleap/archive/v1.0.0.tar.gz',
+    'https://github.com/ConnorSkees/ticketleap/archive/v1.0.1.tar.gz',
     keywords=['ticketleap', 'api'],
     install_requires=[
         'requests',
